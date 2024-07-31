04e4aedc0f68843eb23dc7937a0f067cc401ea40
lhjwork <jin.come.up.business@gmail.com>
2024-07-31 23:03:58 +0900

    상단 Navbar 만들고 선택 후에 선택 활성화 dot 표현

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,49 @@
+import { useState } from "react";
+import { assets } from "../../assets/assets";
+import "./Navbar.css";
+
 const Navbar = () => {
-  return <div className="navbar"></div>;
+  const [menu, setMenu] = useState("home");
+
+  return (
+    <div className="navbar">
+      <img src={assets.logo} alt="" className="logo" />
+      <ul className="navbar-menu">
+        <li
+          onClick={() => setMenu("home")}
+          className={menu === "home" ? "active" : ""}
+        >
+          home
+        </li>
+        <li
+          onClick={() => setMenu("menu")}
+          className={menu == "menu" ? "active" : ""}
+        >
+          menu
+        </li>
+        <li
+          onClick={() => setMenu("mobile-app")}
+          className={menu === "mobile-app" ? "active" : ""}
+        >
+          mobile-app
+        </li>
+        <li
+          onClick={() => setMenu("contact-us")}
+          className={menu === "contact-us" ? "active" : ""}
+        >
+          contact us
+        </li>
+      </ul>
+      <div className="navbar-right">
+        <img src={assets.search_icon} alt="" />
+        <div className="navbar-search-icon">
+          <img src={assets.basket_icon} alt="" />
+          <div className="dot"></div>
+          <button>sign in</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
